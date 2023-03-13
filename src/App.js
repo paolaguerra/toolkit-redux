c5aab7ccb2a63fa430a1e28158f92795177aa160
paolaguerra <paolaguerraph@gmail.com>
2023-03-13 12:47:50 -0400

    Instalacion de react-redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment } from './store/slices/counter/counterSlice';
 
 function App() {
+
+const { counter } = useSelector( state => state.counter )
+const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello React!</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button type="button" onClick={ () => dispatch( increment() ) }>
+          count is: {counter}
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
