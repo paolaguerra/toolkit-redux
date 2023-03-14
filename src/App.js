9823bb0217afb78a2a362c6ffd3b8a8c58fa4e10
paolaguerra <paolaguerraph@gmail.com>
2023-03-14 14:30:01 -0400

    Mostrar una lista a través de un API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increment } from './store/slices/counter/counterSlice';
+import logo from "./logo.svg";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, incrementByTwo } from "./store/slices/counter/counterSlice";
 
 function App() {
-
-const { counter } = useSelector( state => state.counter )
-const dispatch = useDispatch();
+  const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React!</p>
+        <p>count is: {counter}</p>
         <p>
-          <button type="button" onClick={ () => dispatch( increment() ) }>
-          count is: {counter}
+          <button type="button" onClick={() => dispatch(increment())}>
+            Increment
+          </button>
+          <button type="button" onClick={() => dispatch(decrement())}>
+            Decrement
+          </button>
+          <button type="button" onClick={() => dispatch(incrementByTwo(2))}>
+            Increment by 2
           </button>
         </p>
-
       </header>
     </div>
   );
